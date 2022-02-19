f4d2843c01c70a1eed5690d1d93e544406e5b8cf
TesterQATDM <invidia1982@gmail.com>
2022-02-19 21:48:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/chucknorrisjoke/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.chucknorrisjoke
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.chucknorrisjoke.databinding.ActivityMainBinding
+import com.example.chucknorrisjoke.model.ModelService
+import kotlinx.coroutines.*
+import android.net.ConnectivityManager
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnapp.setOnClickListener(){
+            binding.textapp.visibility = View.VISIBLE
+            if (con(this)){
+                CoroutineScope(Dispatchers.Main).launch {
+                    val result = withContext(Dispatchers.IO) {
+                        val helper = ModelService()
+                        return@withContext helper
+                    }
+                    binding.textapp.text = result.mMineUserEntity.value
+
+                }
+            }
+            else{
+                binding.textapp.text = "Проверьте состояние инернета"
+            }
+        }
+    }
+
+    private fun con(context: Context): Boolean{
+        val cm = applicationContext.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        val nInfo = cm.activeNetworkInfo
+        return nInfo != null && nInfo.isAvailable && nInfo.isConnected
+    }
+}
